fab8bc8541e4edbf30f0a50017d5b70b8ed9238a
Zzrk <1252124617@qq.com>
2023-10-22 16:39:48 +0800

    feat: models

---
+++ src/models/calendar.rs
@@ -0,0 +1,20 @@
+use mongodb::bson::oid::ObjectId;
+use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CalendarDocument {
+    pub _id: ObjectId,
+    pub title: String,
+    pub begin_time: String,
+    pub end_time: String,
+    pub description: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Calendar {
+    pub _id: String,
+    pub title: String,
+    pub begin_time: String,
+    pub end_time: String,
+    pub description: String,
+}
